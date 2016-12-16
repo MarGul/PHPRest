1f6c1cdec16567b27de1699f799bd52c636f539b
Marcus Gullberg <MarcusG@HELSINGBORG.OFFICE.SE>
2016-12-16 19:12:43 +0100

    Initial setup

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function store(Request $request) {
+    	return "Store method works";
+    }
+
+    public function signin(Request $request) {
+    	return "Sign in method works";
+    }
+}
